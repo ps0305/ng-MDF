d17598e6c9eddcdc6dccc2c6499ad837c69e2b53
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-05-08 17:35:18 +0530

    Model Driven Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl,Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+private userDetail:FormGroup;
+constructor(){
+        this.userDetails=new FormGroup({
+          fname:new FormControl(),
+          lname:new FormControl(),
+          uemail:new FormControl(),
+          addr:new FormGroup({
+            ucity:new FormControl(),
+            uaddr:new FormControl()
+          }),
+          gender:new FormControl(),
+          ucountry:new FormControl()
+         });
+}
+public register(){
+  console.log(this.userDetail.value);
+}
 }
